0feaf9bacf197cab301a1ddeaa180d205d77f7e1
Nasrul Faizin <faizinnasrul21@gmail.com>
2026-07-08 15:09:06 +0700

    refactor(day-2): split component accordion item

--- 02-accordion/components/Accordion.jsx
+++ 02-accordion/components/Accordion.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import AccordionItem from "./AccordionItem";
 
 export default function Accordion({ items = [] }) {
   const [openedItem, setOpenedItem] = useState();
@@ -16,38 +17,3 @@ export default function Accordion({ items = [] }) {
   )
 }
 
-function AccordionItem({ item, isOpen = false, onItemClick = () => { } }) {
-  return (
-    <div style={styles.accordionItem}>
-      <h4 style={styles.accordionTitle} onClick={onItemClick}>
-        {item.title}
-      </h4>
-      <div style={{ ...styles.accordionSlider, gridTemplateRows: isOpen ? '1fr' : '0fr' }}>
-        <div style={{ overflow: 'hidden' }}>
-          <div style={{ ...styles.accordionContent }}>
-            {item.content}
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
-
-const styles = {
-  accordionItem: {
-    borderBottom: '1px solid var(--primary)',
-  },
-  accordionTitle: {
-    margin: 0,
-    padding: '16px',
-    cursor: 'pointer'
-  },
-  accordionSlider: {
-    display: 'grid',
-    overflow: 'hidden',
-    transition: 'grid-template-rows 0.3s ease-in-out'
-  },
-  accordionContent: {
-    margin: '16px',
-  },
-}
---
+++ 02-accordion/components/AccordionItem.jsx
@@ -0,0 +1,35 @@
+export default function AccordionItem({ item, isOpen = false, onItemClick = () => { } }) {
+  return (
+    <div style={styles.accordionItem}>
+      <h4 style={styles.accordionTitle} onClick={onItemClick}>
+        {item.title}
+      </h4>
+      <div style={{ ...styles.accordionSlider, gridTemplateRows: isOpen ? '1fr' : '0fr' }}>
+        <div style={{ overflow: 'hidden' }}>
+          <div style={{ ...styles.accordionContent }}>
+            {item.content}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const styles = {
+  accordionItem: {
+    borderBottom: '1px solid var(--primary)',
+  },
+  accordionTitle: {
+    margin: 0,
+    padding: '16px',
+    cursor: 'pointer'
+  },
+  accordionSlider: {
+    display: 'grid',
+    overflow: 'hidden',
+    transition: 'grid-template-rows 0.3s ease-in-out'
+  },
+  accordionContent: {
+    margin: '16px',
+  },
+}
